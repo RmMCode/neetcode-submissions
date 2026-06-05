00e0d50c3e5b37ecac702101a4b277480d6dfb19
RmMCode <149433901+RmMCode@users.noreply.github.com>
2026-06-05 12:41:08 -0700

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int notVals = 0; // Where next valid element should be placed
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[notVals] = nums[i];
+                notVals++;
+                // k = nums.length - notVals
+            }
+        }
+        return notVals; // Number of elements not equal to val
+        // System.out.print("k = " + k);
+        // System.out.print(", nums = " + nums);
+    }
+}
